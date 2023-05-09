4208cf2f4dbb9687d7a8f336b7a69b0769fe2d95
Mathieu <mathieu.rasoanaivo@epitech.eu>
2023-05-09 10:21:53 +0400

    steganographie challenge

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vérification de numéro</title>
+</head>
+<body>
+	<h1>Vérification de numéro</h1>
+	<form method="post">
+		<label for="numero">Numéro :</label>
+		<input type="text" id="numero" name="numero" required>
+		<button type="submit">Vérifier</button>
+	</form>
+	<?php
+        $verif = "85691275390";
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$numero = $_POST['numero'];
+			if ($verif === $numero) {
+			    echo "<p>Le numéro est valide. Le flag est epictf{cestpasbiendecouterlesconversationsdesautres}</p>";
+			} else {
+			    echo "<p>Le numéro est invalide.</p>";
+			}
+		}
+	?>
+</body>
+</html>
